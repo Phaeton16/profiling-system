4608c9f9d9ac79df6c2ea5c672fdfb7e3e937632
UlrixStrife <ulrixstrife@gmail.com>
2020-02-25 14:12:22 -0800

    message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -115,7 +115,7 @@
         <div class="col-md-12">
           <h3 class="section-title">About Us</h3>
           <div class="section-title-divider"></div>
-          <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+          <p class="section-description">The Foundation Preparatory Academy nourish students from kinder to senior high school to become a better individual grow with great potential in the future of the society. </p>
         </div>
       </div>
     </div>
@@ -160,22 +160,22 @@
       <div class="row">
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-desktop"></i></div>
-          <h4 class="service-title"><a href="">Lorem Ipsum</a></h4>
+          <h4 class="service-title"><a href="">Vision</a></h4>
           <p class="service-description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-bar-chart"></i></div>
-          <h4 class="service-title"><a href="">Dolor Sitema</a></h4>
+          <h4 class="service-title"><a href="">Mission</a></h4>
           <p class="service-description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-paper-plane"></i></div>
-          <h4 class="service-title"><a href="">Sed ut perspiciatis</a></h4>
+          <h4 class="service-title"><a href="">Life Purpose</a></h4>
           <p class="service-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-photo"></i></div>
-          <h4 class="service-title"><a href="">Magni Dolores</a></h4>
+          <h4 class="service-title"><a href="">Core Values</a></h4>
           <p class="service-description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </div>
         <div class="col-md-4 service-item">
